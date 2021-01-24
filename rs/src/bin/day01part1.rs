c771ee896094116ba5ac866afaede3ca9befde7b
Mike Hoyt <derukimlh@gmail.com>
2021-01-24 07:45:56 -0800

    update src/bin/day01part1.rs so generate_pairs takes a slice (immut ref) instead of the entire Vec (move)
    Although we do not technically need the Vec again after the generate_pairs call (and the move was perfectly fine and probably just as efficient) this change gives more options to the original Vec.

--- rs/src/bin/day01part1.rs
+++ rs/src/bin/day01part1.rs
@@ -5,31 +5,31 @@ fn main() {
         .map(|line| line.parse::<i32>().unwrap())
         .collect::<Vec<_>>();
 
-    let result = generate_pairs(values)
-        .iter()
+    let result = generate_pairs(values.as_slice())
+        .into_iter()
         .filter(|x| x.0 + x.1 == 2020)
         .map(|x| x.0 * x.1)
         .collect::<Vec<_>>();
     println!("{:?}", result);
 }
 
-fn generate_pairs(xs: Vec<i32>) -> Vec<(i32, i32)> {
-    let mut ps = Vec::<(i32, i32)>::new();
+fn generate_pairs(xs: &[i32]) -> Vec<(i32, i32)> {
+    let mut pairs = Vec::<(i32, i32)>::new();
 
     for (xi, x) in xs.iter().enumerate() {
         let ys = &xs[xi + 1..];
         for y in ys.iter() {
-            ps.push((*x, *y));
+            pairs.push((*x, *y));
         }
     }
 
-    return ps;
+    return pairs;
 }
 
 #[test]
 fn test_generate_pairs() {
     let values: Vec<i32> = vec![0, 1, 2];
-    let pairs = generate_pairs(values);
+    let pairs = generate_pairs(values.as_slice());
 
     let expected: Vec<(i32, i32)> = vec![(0, 1), (0, 2), (1, 2)];
     assert_eq!(pairs, expected);
