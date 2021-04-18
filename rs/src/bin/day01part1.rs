ae7a164b751955e208ce9f0389c31ebcb614c340
Mike Hoyt <derukimlh@gmail.com>
2021-04-18 06:41:17 -0700

    refactor rs/ day01part{1,2}.rs to cleanup explicit type declarations

--- rs/src/bin/day01part1.rs
+++ rs/src/bin/day01part1.rs
@@ -1,36 +1,32 @@
 fn main() {
     let input = include_str!("../../input/day01.txt");
-    let values = input
-        .lines()
-        .map(|line| line.parse::<i32>().unwrap())
-        .collect::<Vec<_>>();
+    let numbers: Vec<i32> = input.lines().map(|line| line.parse().unwrap()).collect();
+    let groups = generate_groups(numbers.as_slice());
+    let answers: Vec<_> = groups.into_iter().filter(|x| x.0 + x.1 == 2020).collect();
 
-    let result = generate_pairs(values.as_slice())
-        .into_iter()
-        .filter(|x| x.0 + x.1 == 2020)
-        .map(|x| x.0 * x.1)
-        .collect::<Vec<_>>();
-    println!("{:?}", result);
+    for answer in answers {
+        println!("{} * {} = {}", answer.0, answer.1, (answer.0 * answer.1));
+    }
 }
 
-fn generate_pairs(xs: &[i32]) -> Vec<(i32, i32)> {
-    let mut pairs = Vec::<(i32, i32)>::new();
+fn generate_groups(xs: &[i32]) -> Vec<(i32, i32)> {
+    let mut groups = Vec::new();
 
     for (xi, x) in xs.iter().enumerate() {
         let ys = &xs[xi + 1..];
-        for y in ys.iter() {
-            pairs.push((*x, *y));
+        for y in ys {
+            groups.push((*x, *y));
         }
     }
 
-    return pairs;
+    return groups;
 }
 
 #[test]
-fn test_generate_pairs() {
+fn test_generate_groups() {
     let values: Vec<i32> = vec![0, 1, 2];
-    let pairs = generate_pairs(values.as_slice());
+    let groups = generate_groups(values.as_slice());
 
     let expected: Vec<(i32, i32)> = vec![(0, 1), (0, 2), (1, 2)];
-    assert_eq!(pairs, expected);
+    assert_eq!(groups, expected);
 }
--- rs/src/bin/day01part2.rs
+++ rs/src/bin/day01part2.rs
@@ -1,38 +1,43 @@
 fn main() {
     let input = include_str!("../../input/day01.txt");
-    let values = input
-        .lines()
-        .map(|line| line.parse::<i32>().unwrap())
-        .collect::<Vec<_>>();
-
-    let result = generate_triples(values)
-        .iter()
+    let numbers: Vec<i32> = input.lines().map(|line| line.parse().unwrap()).collect();
+    let groups = generate_groups(numbers.as_slice());
+    let answers: Vec<_> = groups
+        .into_iter()
         .filter(|x| x.0 + x.1 + x.2 == 2020)
-        .map(|x| x.0 * x.1 * x.2)
-        .collect::<Vec<_>>();
-    println!("{:?}", result);
+        .collect();
+
+    for answer in answers {
+        println!(
+            "{} * {} * {} = {}",
+            answer.0,
+            answer.1,
+            answer.2,
+            (answer.0 * answer.1 * answer.2)
+        );
+    }
 }
 
-fn generate_triples(xs: Vec<i32>) -> Vec<(i32, i32, i32)> {
-    let mut ps = Vec::<(i32, i32, i32)>::new();
+fn generate_groups(xs: &[i32]) -> Vec<(i32, i32, i32)> {
+    let mut groups = Vec::new();
 
     for (xi, x) in xs.iter().enumerate() {
         let ys = &xs[xi + 1..];
         for (yi, y) in ys.iter().enumerate() {
             let zs = &ys[yi + 1..];
-            for z in zs.iter() {
-                ps.push((*x, *y, *z));
+            for z in zs {
+                groups.push((*x, *y, *z));
             }
         }
     }
 
-    return ps;
+    return groups;
 }
 
 #[test]
-fn test_generate_triples() {
+fn test_generate_groups() {
     let values: Vec<i32> = vec![0, 1, 2, 3];
-    let groups = generate_triples(values);
+    let groups = generate_groups(values.as_slice());
 
     let expected: Vec<_> = vec![(0, 1, 2), (0, 1, 3), (0, 2, 3), (1, 2, 3)];
     assert_eq!(groups, expected);
