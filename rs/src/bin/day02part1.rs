36f9d4cb1b337a45901a174d347640fb798295f1
Mike Hoyt <derukimlh@gmail.com>
2021-05-04 05:34:37 -0700

    rs: update src/bin/day02part1.rs to use peg and implement FromStr

--- rs/src/bin/day02part1.rs
+++ rs/src/bin/day02part1.rs
@@ -1,59 +1,69 @@
-use regex::Regex;
+use std::str::FromStr;
 
 fn main() {
     let input = include_str!("../../input/day02.txt");
-    let values = input
+    let result = input
         .lines()
-        .map(|line| Password::new(line))
-        .collect::<Vec<_>>();
-
-    let result = values
-        .iter()
-        .filter(|x| match x {
-            Some(p) => p.is_valid(),
-            None => false,
-        })
-        .collect::<Vec<_>>()
-        .len();
-    println!("{:?}", result);
+        .map(|line| line.parse().unwrap())
+        .filter(policy_and_password_is_valid)
+        .count();
+    println!("{}", result);
 }
 
-#[derive(Debug)]
-struct Password {
-    rule_min: usize,
-    rule_max: usize,
-    rule_char: String,
-    value: String,
+pub struct Policy {
+    range_min: usize,
+    range_max: usize,
+    character: u8,
 }
 
-impl Password {
-    fn new(s: &str) -> Option<Password> {
-        let re_password = Regex::new(r"^(\d+)-(\d+)\s+([a-z]):\s+(\S+)$").unwrap();
+pub struct PolicyAndPassword(Policy, String);
 
-        match re_password.is_match(s) {
-            true => {
-                let elems = re_password.captures(s).unwrap();
-                return Some(Password {
-                    rule_min: elems
-                        .get(1)
-                        .map_or(0, |m| m.as_str().parse::<usize>().unwrap()),
-                    rule_max: elems
-                        .get(2)
-                        .map_or(0, |m| m.as_str().parse::<usize>().unwrap()),
-                    rule_char: elems
-                        .get(3)
-                        .map_or(String::new(), |m| m.as_str().to_string()),
-                    value: elems
-                        .get(4)
-                        .map_or(String::new(), |m| m.as_str().to_string()),
-                });
-            }
-            _ => return None,
-        }
+impl FromStr for PolicyAndPassword {
+    type Err = peg::error::ParseError<peg::str::LineCol>;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        parser::Parse(s)
     }
+}
 
-    fn is_valid(&self) -> bool {
-        let n = self.value.matches(self.rule_char.as_str()).count();
-        n >= self.rule_min && n <= self.rule_max
+fn policy_and_password_is_valid(x: &PolicyAndPassword) -> bool {
+    let count = x.1.matches(x.0.character as char).count();
+    count >= x.0.range_min && count <= x.0.range_max
+}
+
+peg::parser! {
+    grammar parser() for str {
+        pub rule Parse() -> PolicyAndPassword
+            = policy:policy() space() password:string()
+              {
+                  PolicyAndPassword(policy, password.to_string())
+              }
+
+        rule policy() -> Policy
+            = range_min:number() "-" range_max:number() space() character:byte() ":"
+              {
+                  Policy{ range_min, range_max, character }
+              }
+
+        rule number() -> usize
+            = n:$(['0'..='9']+)
+              {
+                  n.parse().unwrap()
+              }
+
+        rule byte() -> u8
+            = c:$(['a'..='z'])
+              {
+                  c.as_bytes()[0]
+              }
+
+        rule string() -> &'input str
+            = s:$([_]+)
+              {
+                  s
+              }
+
+        rule space()
+            = [' ']+
     }
 }
